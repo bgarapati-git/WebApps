7bef944d00f77978ac247f26f291bd861b5733ce
bgarapati-git <58849852+bgarapati-git@users.noreply.github.com>
2019-12-16 15:48:04 +0530

    Add files via upload

--- myfirst-angularapp/src/app/messages/messages.component.ts
+++ myfirst-angularapp/src/app/messages/messages.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-messages',
   // selector: '[app-messages]',
   // selector: '.app-messages',
-  templateUrl: './messages.component.html'
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
