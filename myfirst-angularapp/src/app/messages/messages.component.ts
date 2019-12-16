ff884c294836b5a3ecb0d227d9df4bd27e4b9357
bgarapati-git <58849852+bgarapati-git@users.noreply.github.com>
2019-12-16 15:11:32 +0530

    Update messages.component.ts

--- myfirst-angularapp/src/app/messages/messages.component.ts
+++ myfirst-angularapp/src/app/messages/messages.component.ts
@@ -4,8 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-messages',
   // selector: '[app-messages]',
   // selector: '.app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  templateUrl: './messages.component.html'
 })
 export class MessagesComponent implements OnInit {
 
